cbc1ac897bda7ec2878ba507ad4a4f0c5efabf77
gustavo.nunes <105016559+voidGustavoNunes@users.noreply.github.com>
2024-07-11 13:05:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# E-commerceFullStackLove2Code
---
+++ src/test/java/com/luv2Code/springboot_ecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2Code.springboot_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
