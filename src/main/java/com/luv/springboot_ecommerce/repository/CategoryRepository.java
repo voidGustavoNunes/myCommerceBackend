991b0dc7a4c6737f587911db5b00f9e5a4aeda5d
gustavo.nunes <105016559+voidGustavoNunes@users.noreply.github.com>
2024-07-11 20:54:06 -0300

    adding cors treatment

--- src/main/java/com/luv/springboot_ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/luv/springboot_ecommerce/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.luv.springboot_ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv.springboot_ecommerce.model.Category;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "category", path="categories")
 public interface CategoryRepository extends JpaRepository<Category, Long>{
     
--- src/main/java/com/luv/springboot_ecommerce/repository/ProductRepository.java
+++ src/main/java/com/luv/springboot_ecommerce/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.luv.springboot_ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv.springboot_ecommerce.model.Product;
 
-@RepositoryRestResource(collectionResourceRel = "product", path="products")
+@CrossOrigin("http://localhost:4200")
+// @RepositoryRestResource(collectionResourceRel = "product", path="products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
 }
