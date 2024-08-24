beccbdf9d3999e45f95de80cd550f97d0f1582d7
gustavo.nunes <105016559+voidGustavoNunes@users.noreply.github.com>
2024-08-24 18:16:42 -0300

    implementando funcionalidade de pesquisa por categorias em produtos

--- src/main/java/com/luv/springboot_ecommerce/repository/ProductRepository.java
+++ src/main/java/com/luv/springboot_ecommerce/repository/ProductRepository.java
@@ -1,13 +1,17 @@
 package com.luv.springboot_ecommerce.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv.springboot_ecommerce.model.Product;
 
 @CrossOrigin("http://localhost:4200")
-// @RepositoryRestResource(collectionResourceRel = "product", path="products")
+// @RepositoryRestResource(collectionResourceRel = "products", path="products") //colletionResourceRel = oq vem no json, path = oq vem na url ex: localhost:8080/api/products
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
